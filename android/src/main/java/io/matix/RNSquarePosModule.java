9973411a8b5476bb5881a05c28e07cb004a3faeb
Ilya Obshadko <ilyaobshadko@upwork.com>
2024-09-14 20:01:45 -0700

    Fix RN 0.70+ compatibility issues

--- android/src/main/java/io/matix/RNSquarePosModule.java
+++ android/src/main/java/io/matix/RNSquarePosModule.java
@@ -13,6 +13,7 @@ import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
 import com.squareup.sdk.pos.PosClient;
 import com.squareup.sdk.pos.PosSdk;
 import com.squareup.sdk.pos.ChargeRequest;
@@ -56,7 +57,7 @@ class SquarePOSListener implements ActivityEventListener {
 		}
 
 		this.reactContext
-			.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+			.getJSModule(RCTEventEmitter.class)
 			.emit("RNSquarePOSResponse", params);
 	}
 
